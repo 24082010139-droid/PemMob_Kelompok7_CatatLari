9ecb7c5b2fda2c2659e7782a2c609a6c3964027e
Sarah Amelia Rachma <24082010139@student.upnjatim.ac.id>
2026-03-04 09:41:58 +0700

    Initial commit

---
+++ app/src/main/java/com/ubermensch/larilari/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.ubermensch.larilari
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.fragment_catat_lari)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.catat_lari)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
